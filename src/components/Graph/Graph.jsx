20992c95292e3873d92c850b0c6fd48f24a0f856
poorvanandanwar <poorvanandanwar4@gmail.com>
2025-02-08 23:14:36 +0530

    dashboard

---
+++ src/components/Graph/Graph.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Graph.css'
+import dlwd from '../../assets/dld.png'
+import graphimg from '../../assets/graphhh.jpeg'
+const Graph = () => {
+  return (
+    <div className='forecast'>
+        <h2>RENEWABLE ENERGY FORECASTING DASHBOARD</h2>
+        <div className="content">
+            <div className="overview">
+                <div className="type">
+                    <ul>
+                        <li>OVERVIEW</li>
+                        <li>SOLAR</li>
+                        <li>WIND</li>
+                        <li>HYDRO</li>
+                    </ul>
+                </div>
+                <div className="type">
+                    <ul>
+                        <li>TODAY</li>
+                        <li>LAST 7 DAYS</li>
+                        <li>LAST MONTH</li>
+                    </ul>
+                </div>
+                <div className="download">
+                    <button className='dld'>DOWNLOAD REPORT <img src={dlwd} alt="" /></button>
+                </div>
+            </div>
+            <div className="grph"><img src={graphimg} alt="" /></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Graph
